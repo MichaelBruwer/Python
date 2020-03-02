706f3b8fe0fac34e8684c6d32dad27b5c367133f
Thandolwethu Njili <thandolnjili@gmail.com>
2020-03-02 13:01:34 +0200

    Python loop

---
+++ Intro.py
@@ -0,0 +1,27 @@
+
+    #ex1
+
+#num = int(input("Enter a number: "))
+#if (num % 2) == 0:
+ #  print("Even number")
+#if (num % 3) == 0:
+ #   print ("Odd number")
+#
+    #FOR LOOP
+  # for x in range (100):
+    #   print(x)
+
+for x in range (1,101):
+
+    if  (x % 3) ==0 :
+        print ("Fizz")
+    if (x  % 5)  ==0:
+        print ("Buzz")
+    if (x % 3) ==0 and (x % 5) ==0:
+        print ("FizzBuzz")
+    print (x)
+
+
+
+
+
